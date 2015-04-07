00f760e2b02b77dfedbc620e32ac354ff5dd5019
Axel CÉARD <axel.ceard@iut-valence.fr>
2015-04-07 10:50:00 +0200

    Javadoc team

--- src/fr/iut/tapawaru/team/Character.java
+++ src/fr/iut/tapawaru/team/Character.java
@@ -1,5 +1,11 @@
 package fr.iut.tapawaru.team;
 
+/**
+ * Personnage d'une équipe.
+ * Contenu dans l'attribut listCharacter.
+ * 
+ * @author Axce
+ */
 public class Character
 {
 	
--- src/fr/iut/tapawaru/team/Team.java
+++ src/fr/iut/tapawaru/team/Team.java
@@ -1,6 +1,47 @@
 package fr.iut.tapawaru.team;
 
+/**
+ * Équipe de persos. 2 équipes s'affrontent dans une partie. Une équipe contient
+ * un nombre de persos déterminé au début de la partie (à confirmer). Chaque
+ * équipe a une couleur.
+ * 
+ * @author Axce
+ */
 public class Team
 {
+	// TODO un enum des couleurs possibles.
 	
+	public final static int	DEFAULT_NB_CHARACTER	= 3;
+	
+	/**
+	 * Nombre de persos dans la Team.
+	 * À mettre en static si il doit être identique pour les deux équipes.
+	 */
+	private final int		nbCharacter;
+	
+	/**
+	 * Liste des persos.
+	 */
+	private Character[]		listCharacter;
+	
+	/**
+	 * Constructeur Team.
+	 * 
+	 * @param nb	Le nombre de persos.
+	 */
+	public Team(int nb)
+	{
+		nbCharacter = nb;
+		listCharacter = new Character[nbCharacter];
+	}
+	
+	/**
+	 * Constructeur default Team.
+	 * 
+	 * @param nb	Le nombre de persos.
+	 */
+	public Team()
+	{
+		this(DEFAULT_NB_CHARACTER);
+	}
 }
