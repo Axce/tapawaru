055f9e9bedfeb7351b7ddbc0d2fcc69a0b9c7b72
olivier <olivier@Olivier.home>
2015-05-21 15:11:51 +0100

    creation des attributs, constructeur et methode isAlive() faire methode kill et castSpell

--- src/fr/iut/tapawaru/team/Character.java
+++ src/fr/iut/tapawaru/team/Character.java
@@ -8,5 +8,47 @@ package fr.iut.tapawaru.team;
  */
 public class Character
 {
+ /**
+  * team d'appartenance du character	
+  */
+	private Team team;
 	
+ /**
+  * point de vie du character 
+  */
+	private int healthPoint;
+	
+  public Character(Team team, int healthpoint)
+  {
+	  this.team = team;
+	  this.healthPoint = healthpoint;
+  }
+  
+  /**
+   * 
+   */
+  public void castSpell()
+  {
+	  //TODO  faire la méthode
+  }
+  
+  /**
+   * test si le character est en vie 
+   */
+  public boolean isAlive()
+  {
+	  if (this.healthPoint <= 0)
+		  return false;
+	  return true;
+  }
+  
+  /**
+   * supprime l'objet character courant
+   */
+  public void kill()
+  {
+	  if (!this.isAlive())
+		  //TODO faire la méthode;
+  }
 }
+
