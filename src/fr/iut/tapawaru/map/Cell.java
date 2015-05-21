c1f6a6a97485850235cca70e1f952f04511f3e2c
pradello <pradello@PC-B103-03.iut-valence.fr>
2015-05-21 11:50:46 +0200

    Complement sur la classe cell (terminer mais à vérifier)  et classe map (voire le TODO)  ps : jules et axcel voir avec tom pour les glyphes

--- src/fr/iut/tapawaru/map/Cell.java
+++ src/fr/iut/tapawaru/map/Cell.java
@@ -9,5 +9,58 @@ package fr.iut.tapawaru.map;
  */
 public class Cell extends MapElement
 {
+	/**
+	 * positionX d'un cell sur la map
+	 */
+	private int positionX;
+	
+	/**
+	 * positionY d'un cell sur la map
+	 */
+	private int positionY;
+	
+	public Cell(int positionX, int positionY)
+	{
+		this.positionX = positionX;
+		this.positionY = positionY;
+	}
+
+	/**
+	 * obtention de la positionX de la cell courante
+	 * @return positionX courante
+	 */
+	public int getPositionX() 
+	{
+		return this.positionX;
+	}
+	
+	/**
+	 * modification de la positionX de la cell
+	 * @param int positionX
+	 */
+	public void setPositionX(int positionX) 
+	{
+		this.positionX = positionX;
+	}
+	
+	/**
+	 * oBtention de la positionY de la cell courante
+	 * @return positionY courante
+	 */
+	public int getPositionY() 
+	{
+		return positionY;
+	}
+
+	/**
+	 * modification de la positionY de la cell 
+	 * @param int positionY
+	 */
+	public void setPositionY(int positionY) 
+	{
+		this.positionY = positionY;
+	}
+	
+	//TODO vérifier le code
 	
 }
--- src/fr/iut/tapawaru/map/Map.java
+++ src/fr/iut/tapawaru/map/Map.java
@@ -8,5 +8,37 @@ package fr.iut.tapawaru.map;
  */
 public class Map
 {
+	/**
+	 * longueur de la map
+	 */
+	private final int length;
+	
+	/**
+	 * largeur de la map
+	 */
+	private final int width;
+	
+	/**
+	 * grille des cell
+	 */
+	private final Cell[][] map;
+	
+	
+	/**
+	 * creation de la map
+	 */
+	public Map()
+	{
+		this.map = new Cell[this.length][this.width];
+		for(int lineNumber =0; lineNumber<this.length;lineNumber++)
+		{
+			for(int columnNumber = 0; columnNumber < this.width; columnNumber++)
+			{
+				this.map[lineNumber][columnNumber]= new Cell(lineNumber,columnNumber);
+			}
+		}
+	}
 	
 }
+
+//TODO vérifier le code est voire les dimension de la map (modifiable ou nn ?)
